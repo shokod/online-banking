ae90cb3c56f61935bb13efe013c0b2c0a6b990e5
shokod <delvinshoko@gmail.com>
2024-05-11 00:14:15 +0200

    Implement headerbox display with countup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 function Home() {
+    const loggedIn = { firsName: 'Delvin'}
+
+
   return (
-    <div> Home</div>
+    <section className='home'>
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firsName || "Guest"}
+                subtext="Access and manage your account and transactions from anywhere in the world with ease."
+                />
+                <TotalBalanceBox
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={12458.30}
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+'use client'
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({amount}: {amount: number}) => {
+  return (
+    <div className='w-full'>
+        <CountUp
+            decimals={2}
+            decimal=','
+            prefix='$'
+            duration={1.5}
+            end={amount}
+        />
+    </div>
+  )
+}
+
+export default AnimatedCounter
